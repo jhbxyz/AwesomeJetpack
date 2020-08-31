6b9afdbcbb1f088da4b0d6292cd3e11284e5eaf3
jianghaibo <jhbxyz@163.com>
2020-08-31 17:27:41 +0800

    view model

---
+++ app/src/main/java/com/jhb/awesomejetpack/viewmodel/UserViewModel.kt
@@ -0,0 +1,33 @@
+package com.jhb.awesomejetpack.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.util.*
+
+/**
+ * @author jhb
+ * @date 2020/8/31
+ */
+//自定义 User 数据类
+data class User(var userId: String = UUID.randomUUID().toString(), var userName: String)
+
+class UserViewModel : ViewModel() {
+
+    private val userBean = User(userName = "刀锋之影")
+    // 私有的 user LIveData
+    private val _user = MutableLiveData<User>().apply {
+        value = userBean
+    }
+    // 对外暴露的,不可更改 value 值的LiveData
+    var userLiveData: LiveData<User> = _user
+
+    //更新 User 信息
+    fun updateUser() {
+        //重新给 _user 赋值
+        _user.value = userBean.apply {
+            userId = UUID.randomUUID().toString()
+            userName = "更新后: userName = 泰隆"
+        }
+    }
+}
---
+++ app/src/main/java/com/jhb/awesomejetpack/viewmodel/ViewModelActivity.kt
@@ -0,0 +1,34 @@
+package com.jhb.awesomejetpack.viewmodel
+
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.jhb.awesomejetpack.util.log
+
+/**
+ * @author jhb
+ * @date 2020/8/31
+ */
+class ViewModelActivity : AppCompatActivity() {
+
+    //初始化 UserViewModel 通过 ViewModelProvider
+    private val userViewModel by lazy { ViewModelProvider(this)[UserViewModel::class.java] }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val button = Button(this)
+        setContentView(button)
+
+        //观察 User 数据,并打印
+        userViewModel.userLiveData.observe(this, Observer { user ->
+            "User = $user".log()
+        })
+
+        //点击按钮更新 User 信息
+        button.setOnClickListener {
+            userViewModel.updateUser()
+        }
+    }
+}
